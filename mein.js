b86da468fcac311cc66cfa7cfd7cfc719f2fa19e
sodadayo20 <sodadayo20@gmail.com>
2022-12-10 14:40:33 +0900

    hot fixing...

--- mein.js
+++ mein.js
@@ -2,6 +2,12 @@ import './src/mp3.js';
 
 // ./icons/vllage3E.png
 const load = async () => {
+    let iframe = document.getElementsByTagName('iframe')[0];
+    iframe.onload = () => {
+        console.log('show?')
+        iframe.classList.toggle('show');
+        document.querySelector('main.chat').classList.toggle('show');
+    }
     const setups = await (await fetch('./data.json')).json();
     const imageSetup = setups.emojiSetup;
     const musicObj = setups.musics;
@@ -10,11 +16,6 @@ const load = async () => {
         op.value = e[0];
         op.innerHTML = e[1];
         document.querySelector('select#trackList').append(op);
-        let iframe = document.getElementsByTagName('iframe')[0];
-        iframe.onload = () => {
-            iframe.classList.toggle('show');
-            document.querySelector('main.chat').classList.toggle('show');
-        }
     })
     const ir = new imgRep(document.querySelector('.cont main'), imageSetup)
     ir.replaceAll();
